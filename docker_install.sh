fd15f398e6fdfc91d0904aab9a8aadd97c280f23
devd4n <devd4n@local.local>
2025-03-08 16:09:08 +0100

    update to also support debian

--- docker_install.sh
+++ docker_install.sh
@@ -2,12 +2,20 @@
 
 DOCKER_USERNAME=docker_user
 
+# Überprüfe die Distribution (Ubuntu oder Debian)
+if [ -f /etc/debian_version ]; then
+    DISTRO=$(lsb_release -is)
+else
+    echo "Nicht unterstützte Distribution"
+    exit 1
+fi
+
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$DISTRO $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo useradd -m $DOCKER_USERNAME -s /bin/bash
